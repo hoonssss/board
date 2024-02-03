bc334c96a8c83d43725c935ba37ae69a1aee4ea6
JaeHoon <wogns8030@naver.com>
2024-02-03 12:56:16 +0900

    Error

--- src/test/java/com/example/projectboard/BoardApplicationTests.java
+++ src/test/java/com/example/projectboard/BoardApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
-@Configuration
 class BoardApplicationTests {
 
     @Test
