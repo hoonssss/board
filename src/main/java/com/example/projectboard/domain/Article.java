9d993ff77dee5c603c0b624e29cb6795f9a11f85
JaeHoon <wogns8030@naver.com>
2024-02-03 12:00:07 +0900

    #10 - Domain Code Entity 변환
    도메인 클래스 테이블 매칭되도록 설계
    생성자와 equals(), hashcode() 등 설정

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -1,18 +1,92 @@
 package com.example.projectboard.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+
+@Getter
+@ToString
+@Table(indexes = {
+    @Index(columnList = "title"),
+    @Index(columnList = "hashtag"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; //제목
-    private String content; //내용
-    private String hashtag; //해시태그
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @Column(nullable = false) private String title; //제목
+    @Setter @Column(nullable = false, length = 10000) private String content; //내용
 
+    @Setter private String hashtag; //해시태그
 
+    @OrderBy("id")
+    @ToString.Exclude
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Article article)) {
+            return false;
+        }
+        /**
+         * 비교 시 id 값으로 충분
+         * 영속화 되지 않은 id는 id 값이 null일 수 있음
+         * id != null && id.equals(article.id) -> 영속화되지 않은 Entity는 모두 동등성 검사에서 탈락함을 뜻함
+         **/
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
