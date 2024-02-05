577060a0dd52f8d062ea48ed281a2b5179fac329
JaeHoon <wogns8030@naver.com>
2024-02-05 11:18:21 +0900

    #17 - 게시글 관련 뷰 테스트 작성

---
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@DisplayName("view 컨트롤러")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 -> 정상 호출 확인")
+    @Test
+    void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("articles")); //articles date가 있는지
+    }
+
+    @DisplayName("[view][GET] 게시글 단일 (게시판) 페이지 -> 정상 호출 확인")
+    @Test
+    void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("article")); //articles date가 있는지
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 (게시판) 페이지 -> 정상 호출 확인")
+    @Test
+    void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 (게시판) 페이지 -> 정상 호출 확인")
+    @Test
+    void givenNothing_whenRequestingArticleSearchHashtagView_thenReturnsArticleSearchHashtagView() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+}
